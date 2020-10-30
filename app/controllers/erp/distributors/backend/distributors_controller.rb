d1f5f64ce2d0756feb195376edae1d001c569094
hoangtularva <hoangtukg295@gmail.com>
2020-10-30 19:47:18 +0700

    update status in distributors

--- app/controllers/erp/distributors/backend/distributors_controller.rb
+++ app/controllers/erp/distributors/backend/distributors_controller.rb
@@ -39,8 +39,8 @@ module Erp
             if request.xhr?
               render json: {
                 status: 'success',
-                text: @distributor.id,
-                value: @distributor.name
+                text: @distributor.name,
+                value: @distributor.id
               }
             else
               redirect_to erp_distributors.edit_backend_distributor_path(@distributor), notice: 'Distributor was successfully created.'
@@ -60,8 +60,8 @@ module Erp
             if request.xhr?
               render json: {
                 status: 'success',
-                text: @distributor.id,
-                value: @distributor.name
+                text: @distributor.name,
+                value: @distributor.id
               }
             else
               redirect_to erp_distributors.edit_backend_distributor_path(@distributor), notice: 'Distributor was successfully updated.'
