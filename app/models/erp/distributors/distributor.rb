0ae102b6e6471e379059ee66cf02ea7c1030a3f3
Hung Nguyen <mrhungton@gmail.com>
2021-01-05 14:29:42 +0700

    update search/filter

--- app/models/erp/distributors/distributor.rb
+++ app/models/erp/distributors/distributor.rb
@@ -77,8 +77,32 @@ module Erp::Distributors
 
         query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
 
+        # single keyword
+        if params[:keyword].present?
+          keyword = params[:keyword].strip.downcase
+          keyword.split(' ').each do |q|
+            q = q.strip
+            query = query.where('LOWER(erp_distributors_distributors.name) LIKE ? OR LOWER(erp_distributors_distributors.name) LIKE ? OR LOWER(erp_distributors_distributors.name) LIKE ?', q+'%', '% '+q+'%', '%-'+q+'%')
+          end
+        end
+
         return query
       end
+    end
+
+    def self.search(params)
+      query = self.all
+      query = self.filter(query, params)
+
+      # order
+      if params[:sort_by].present?
+        order = params[:sort_by]
+        order += " #{params[:sort_direction]}" if params[:sort_direction].present?
+
+        query = query.order(order)
+      end
+
+      return query
     end
 
     # data for dataselect ajax
